dccd8e5b77fa8082ad782b690dbbff54cfa7cd71
刘松 <sidliu@kugou.net>
2022-02-18 17:28:56 +0800

    更新函数使用策略

--- SwiftRecord.playground/Pages/Function.xcplaygroundpage/Contents.swift
+++ SwiftRecord.playground/Pages/Function.xcplaygroundpage/Contents.swift
@@ -159,6 +159,163 @@ let myArr = [3,1,2]
 //: **接受两个对象作为参数，并在他们顺序正确的时候返回true**
 myArr.sorted(by: <)     // 从小到大
 
+// 就算待排序的元素不遵守Compareable, 但只要有<操作符，就可以使用这个方法来进行排序，比如元组
+var numberStrings = [(2, "two"), (1, "one"), (3,"three")]
+numberStrings.sort(by: <)
+
+// 或者也可以使用一个更复杂的函数，按照任意你需要的计算标准进行排序
+
+let animals = ["elephat", "zebra","dog"]
+animals.sorted { left , right in
+    let l = left.reversed() // 反序
+    let r = right.reversed()
+    return l.lexicographicallyPrecedes(r);// 内部使用<来对比
+}
+
+
+// 最后Swift的排序还有一个能力，它可以使用任意的比较函数来对集合进行排序。这使得Swift排序非常强大
+
+
+
+// 接下来展示Objective-C强大的运行时来进行排序的工作方式
+@objcMembers
+final class Person: NSObject {
+    let first:String
+    let last:String
+    let yearOfBirth:Int
+    
+    init(first:String, last:String, yearOfBirth:Int) {
+        self.first = first
+        self.last = last
+        self.yearOfBirth = yearOfBirth
+        
+        // super.init() 会进行隐式调用
+    }
+    
+    
+    override var description: String {
+        return "(\(first) \(last) (\(yearOfBirth)))\n"
+    }
+}
+
+
+// 定义一个数组，包含不同的名字和出生年份的人
+let people = [Person.init(first: "Emily", last: "Young", yearOfBirth: 2002),
+              Person.init(first: "David", last: "Gray", yearOfBirth: 1991),
+              Person.init(first: "Robert", last: "Barnes", yearOfBirth: 1985),
+              Person.init(first: "Ava", last: "Barnes", yearOfBirth: 2000),
+              Person.init(first: "Joanne", last: "Miller", yearOfBirth: 1994),
+              Person.init(first: "Ava", last: "Barnes", yearOfBirth: 1998)
+]
+
+
+// 我们需要的规则： 1. 姓排序，2. 名排序， 3. 出生年份    备注：last: 姓  first:名
+
+
+// 使用NSSortDescriptor对象来描述如何排序对象
+
+// 姓氏排序
+let lastDescriptor = NSSortDescriptor.init(key: #keyPath(Person.last),
+                                           ascending: true,
+                                           selector: #selector(NSString.localizedStandardCompare(_:)))
+// 名排序
+let firstDescriptor = NSSortDescriptor.init(key: #keyPath(Person.first),
+                                            ascending: true,
+                                            selector: #selector(NSString.localizedStandardCompare(_:)))
+// 生日排序
+let yearDescriptor = NSSortDescriptor.init(key: #keyPath(Person.yearOfBirth), ascending: true)
+
+// 使用数组的描述进行排序
+let descriptors = [lastDescriptor, firstDescriptor, yearDescriptor]
+
+let y = (people as NSArray).sortedArray(using: descriptors)
+
+
+// 排序描述符用到了Objective-C的两个运行时特性；
+// 首先key是Objective-C的键路径，它其实是一个包含属性名字的量表。不要把它和Swift4引入的原生的(强类型的)键路径搞混
+// 其次是 键值编程（key-value-coding），它可以在运行时通过键查找一个对象上的对应值。
+// selector参数接受一个selector（实际上也是一个用来猫叔方法名字的字符串）
+// 在运行时，这个selector会被用来查找比较函数，当对两个对象进行比较时，这个函数将使用指定键对应的值进行比较。
+
+// 这是运行时变成的一个很酷的用例，排序描述的数组可以在运行时构建，这一点在实现比如用户点击某一列时按照该列进行排序这种需求时会特别有用
+
+
+// 那用Swift的sort怎么来赋值这个功能呢？
+
+var strings = ["Hello", "hallo", "Hallo", "hello"]
+strings.sort{$0.localizedStandardCompare($1) == .orderedAscending}
+//strings
+
+// 如果是想用队形的某一个属性进行排序的haul，也简单
+let sortPerson = people.sorted{$0.yearOfBirth < $1.yearOfBirth}
+
+// 不过，当要把可选值属性与像是localizedStandardCompare这样的方法结合起来使用的话，这条路就有点走不通了，代码会迅速变得丑陋不堪 示例：
+//var files = ["one", "file.h", "file.c", "test.h"]
+//files.sort { l, r in
+//    r.fileExtension.flatMap {
+//        l.fileExtension?.localizedStandardCompare($0)
+//    } == .orderedAscending
+//}
+
+// 要同时排序姓和名，我们可以用标准库的lexicographicallyPrecedes方法来进行实现。
+// 这个方法接收两个序列,并对它们执行一个电话簿的方式比较， 也就是说，这个比较将顺次从两个序列中各取一个元素进行比较，知道发现不相等的元素。
+// 所以我们可以用姓和名构建两个数组，然后使用lexicographicallyPrecedes来比较它们。我们还需要一个函数来执行这个比较，这里我们把使用了localizedStandardCompare的比较代码放到这个函数中：
+
+people.sorted { p0 , p1 in
+    let l = [p0.last, p0.first]
+    let r = [p1.last, p1.first]
+    return l.lexicographicallyPrecedes(r) {
+        $0.localizedStandardCompare($1) == .orderedAscending
+    }
+}
+
+// 至此，我们用了差不多相同的行数重新实现了原来的那个排序方法，不过还有很大的改进空间：
+// 在每次比较的时候都构建一个数组是非常没有效率的，比较操作也是被写死的，通过这种实现无法对yearOfBirth进行排序
+
+
+//sortPerson.forEach { person in
+//    print(person)
+//}
+
+//: ## 函数作为数据
+
+// 其实排序最好的定义方式就是入参两个，当顺序一致时返回true
+typealias SortDescriptor<Root> = (Root, Root) -> Bool
+
+
+// 接下来使用函数的方式定义描述符
+let sortByYear:SortDescriptor<Person> = {$0.yearOfBirth < $1.yearOfBirth}
+let sortByLastName:SortDescriptor<Person> = {$0.last.localizedStandardCompare($1.last) == .orderedAscending}
+let sortByFirstName:SortDescriptor<Person> = {$0.first.localizedStandardCompare($1.first) == .orderedAscending}
+
+// 为了避免复制粘贴时，容易造成.first与.last进行比较导致的问题, 可以进行更进一步的封装
+
+func sortDescriptor<Root, Value> (key: @escaping (Root)->Value, by areInIncreasingOrder:@escaping (Value, Value) -> Bool) -> SortDescriptor<Root>
+{
+    return {areInIncreasingOrder(key($0), key($1))}
+}
+
+// key函数描述了如何深入一个Root类型的元素，并提取出一个和特定排序步骤相关的Value类型的值。因为借鉴了泛型参数名字Root和Value
+// 所以它和Swift4引入的Swift原生键路径有很多相同之处。我们会再下面讨论怎么用Swift的键值路径重写这个方法
+
+
+// 有了上述方法，就可以定义sortByYear了
+let sortByYearAlt:SortDescriptor<Person> = sortDescriptor(key: {$0.yearOfBirth}, by: <)
+
+let mySort = people.sorted(by: sortByYearAlt)
+
+mySort.forEach { person in
+    print(person)
+}
+
+// 甚至还可以为所有实现了Comparable的类型定义一个重载版本：
+func sortDescriptor<Root, Value>(key:@escaping (Root)-> Value)
+-> SortDescriptor<Root> where Value: Comparable {
+    return {key($0) < key($1)}
+}
+
+let sortByYearAlt2: SortDescriptor<Person> = sortDescriptor(key: {$0.yearOfBirth})
+
 
 
 //: ---
